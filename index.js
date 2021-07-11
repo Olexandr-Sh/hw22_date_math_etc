c4ad9fe689bcc83201c3d00c5659bb078a2338d5
Olexandr-Sh <shevtcov07@ukr.net>
2021-07-12 01:07:15 +0300

    finished the Heads and tails game

--- index.js
+++ index.js
@@ -15,7 +15,55 @@ console.log("Average popularity:", averagePopularity);
 
 let start = new Date();
 
-let birthday = new Date(1995, 05, 16, 0, 0);
+let birthday = new Date(1995, 05, 16, 0, 0, 0);
 
 console.log(start === birthday);
 console.log(birthday - start);
+
+let timestamp = birthday.getTime();
+let checkNow = new Date(timestamp);
+
+console.log(checkNow);
+
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".inner-box");
+let flipBtn = document.querySelector(".flip-button");
+let resetBtn = document.querySelector(".reset-button");
+
+flipBtn.addEventListener("click", () => {
+  let i = Math.floor(Math.random() * 2);
+  coin.style.animation = "none";
+  if (i) {
+    setTimeout(function () {
+      coin.style.animation = "spin-circle-head 3s forwards";
+    }, 100);
+    heads++;
+  }
+  else {
+    setTimeout(function () {
+      coin.style.animation = "spin-circle-tails 3s forwards";
+    }, 100);
+    tails++;
+  }
+  console.log(i);
+  setTimeout(updateStats, 3000);
+  disableButton();
+});
+function updateStats() {
+  document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+  document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+  
+}
+function disableButton() {
+  flipBtn.disabled = true;
+  setTimeout(function () {
+    flipBtn.disabled = false;
+  }, 3000);
+}
+resetBtn.addEventListener("click", () => {
+  coin.style.animation = "none";
+  heads = 0;
+  tails = 0;
+  updateStats();
+})
